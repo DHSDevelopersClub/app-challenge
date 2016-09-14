1fe3b3da580c1aff3533bb3940156aec8050408a
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2016-09-13 18:03:28 -0700

    add more data to users

--- server/messages.py
+++ server/messages.py
@@ -9,9 +9,18 @@ class Status(messages.Enum):
     EXISTS = 3
     BAD_DATA = 4
     ERROR = 5
+    NOT_LOGGED_IN = 6
 
-class AddUser(messages.Message):
+class EditUser(messages.Message):
+    '''Edit user or update profile'''
+    class Sex(messages.Enum):
+        MALE = 0
+        FEMALE = 1
+        OTHER = 2
     name = messages.StringField(1, required=True)
+    age = messages.IntegerField(2, required=True)
+    sex = messages.EnumField(Sex, 3, required=True)
+
 
 class StatusMessage(messages.Message):
     status = messages.EnumField(Status, 1, required=True)
--- server/models.py
+++ server/models.py
@@ -6,3 +6,6 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     '''One user'''
     account = ndb.UserProperty()
+    name = ndb.StringProperty()
+    age = ndb.IntegerProperty()
+    sex = ndb.EnumProperty()

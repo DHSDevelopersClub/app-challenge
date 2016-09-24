f572b4d51665cdc508b067ebe544240736980c60
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-09-23 23:02:57 -0700

    Connect client to API!

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,18 @@
+function loaded() {
+  console.log('loadeded');
+}
+
+var server;
+
 var backend = document.getElementById('backend');
-
-var google = google || {};
-
-console.log(gapi.backend);
+  backend.addEventListener('google-api-load', function(event) {
+    server = backend.api;
+    request = server.get_activities({
+        lat: 34,
+        lng: 34,
+    });
+    request.execute(function(resp) {
+      console.log('resp: ');
+      console.log(resp.activites[1].user_created_disrcription);
+    });
+});

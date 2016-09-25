2fdf7868cbb586a88c38088e431641cd58f38f0f
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-09-25 15:31:27 -0700

    Connect to API, add homepage.

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,2 @@
-function loaded() {
-  console.log('loadeded');
-}
-
-var server;
-
-var backend = document.getElementById('backend');
-  backend.addEventListener('google-api-load', function(event) {
-    server = backend.api;
-    request = server.get_activities({
-        lat: 34,
-        lng: 34,
-    });
-    request.execute(function(resp) {
-      console.log('resp: ');
-      console.log(resp.activites[1].user_created_disrcription);
-    });
-});
+var app = document.querySelector('#app');
+console.log(app);

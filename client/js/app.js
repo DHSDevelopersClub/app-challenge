16ba2c39993881663a6ebbc9c9dc39f63720ae97
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-10-01 19:36:41 -0700

    Add data connection, distance var.

--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,34 @@
 var backend = document.getElementById('backend');
+var $list = document.getElementsByTagName('my-app')[0];
+meta = document.querySelector('#meta');
 
 var google = google || {};
 
-console.log(gapi.backend);
+var dataEvent; // The custom Event that will be created
+ if (document.createEvent) {
+   dataEvent = document.createEvent("HTMLEvents");
+   dataEvent.initEvent("data-available", true, true);
+ } else {
+   dataEvent = document.createEventObject();
+   dataEvent.eventType = "data-available";
+ }
+ dataEvent.eventName = "data-available";
 
-meta = document.querySelector('#meta');
-meta.value = {'a':'b'};
-var app = document.querySelector('#app');
-console.log(app);
+var server;
+
+var backend = document.getElementById('backend');
+  backend.addEventListener('google-api-load', function(e) {
+    server = backend.api;
+    request = server.get_activities({
+        lat: 34,
+        lng: 34,
+    });
+    request.execute(function(resp) {
+      meta.value = resp.activites;
+      if (document.createEvent) {
+        document.dispatchEvent(dataEvent);
+      } else {
+        document.fireEvent("on" + dataEvent.eventType, dataEvent);
+      }
+    });
+});

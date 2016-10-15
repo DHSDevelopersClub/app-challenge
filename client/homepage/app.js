a55ae571e560589ef896735cec298889eae20eb7
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-10-14 17:35:40 -0700

    Fix homepage.

--- client/homepage/app.js
+++ client/homepage/app.js
@@ -1,91 +1,11 @@
-var list = {
-    "description": "A list of English verbs.",
-    "verbs": [
-        {
-            "present": "hike",
-            "past": "accepted"
-        },
-        {
-            "present": "surf",
-            "past": "added"
-        },
-        {
-            "present": "snowboard",
-            "past": "admired"
-        },
-        {
-            "present": "climb",
-            "past": "admitted"
-        },
-        {
-            "present": "bike",
-            "past": "advised"
-        },
-        {
-            "present": "skate",
-            "past": "afforded"
-        },
-        {
-            "present": "scooter",
-            "past": "agreed"
-        },
-        {
-            "present": "run",
-            "past": "alerted"
-        },
-        {
-            "present": "read",
-            "past": "allowed"
-        },
-        {
-            "present": "bake",
-            "past": "amused"
-        },
-        {
-            "present": "cook",
-            "past": "analysed"
-        },
-        {
-            "present": "play",
-            "past": "announced"
-        },
-        {
-            "present": "skydive",
-            "past": "annoyed"
-        },
-        {
-            "present": "longboard",
-            "past": "answered"
-        },
-        {
-            "present": "bmx",
-            "past": "apologised"
-        },
-        {
-            "present": "sing",
-            "past": "appeared"
-        },
-        {
-            "present": "dance",
-            "past": "applauded"
-        },
-        {
-            "present": "swim",
-            "past": "appreciated"
-        },
-        {
-            "present": "skimboard",
-            "past": "approved"
-        }
-    ]
-}
+var list = {"verbs": ['fun', 'creative', 'new', 'active', 'awesome', 'new', 'great', 'super', 'similar', 'athletic', 'smart']}
 
-var $verb = document.querySelector('#verb');
+var $adjective = document.querySelector('#adjective');
 
 function printRandom(data) {
   'use strict';
   var i = Math.floor((Math.random() * list.verbs.length));
-  $verb.textContent = "Let's " + list.verbs[i].present;
+  $adjective.textContent = 'An app for finding ' + list.verbs[i] + ' friends.';
 }
 
 window.setInterval("printRandom()", 900);
